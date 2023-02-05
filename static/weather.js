2c4adb185a7e064d157a7a39aa67b4fc18b4d2d7
Rada Berar <ujagaga@gmail.com>
2023-02-05 17:34:07 +0100

    Add video transition

--- static/weather.js
+++ static/weather.js
@@ -4,6 +4,27 @@ function reload() {
     location.href = address;
 }
 
+function setupVideoQueue(start_id, end_id){
+    var startVideo = document.getElementById(start_id);
+    var endVideo = document.getElementById(end_id);
+
+    var duration = (startVideo.duration / startVideo.playbackRate - 2) * 1000;
+
+    startVideo.classList.add('showing');
+    startVideo.classList.remove('fading');
+    endVideo.classList.add('fading');
+    endVideo.classList.remove('showing');
+
+    setTimeout(() => {
+        startVideo.classList.remove('showing');
+        startVideo.classList.add('fading');
+        endVideo.classList.remove('fading');
+        endVideo.classList.add('showing');
+        endVideo.play();
+    }, duration)
+}
+
+
 window.onload = function() {
     // Refresh every half an hour
     setTimeout(reload, 1800000);
